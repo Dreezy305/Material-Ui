53fae4172cf83f73190af8f4056bb05054578a07
Dreezy305 <bankoleidris@gmail.com>
2021-08-07 13:05:15 +0000

    importing components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import "./App.css";
-import { Typography } from "@material-ui/core/Typography";
+import {
+  Typography,
+  Card,
+  CardActions,
+  AppBar,
+  CardContent,
+  CardMedia,
+  CssBaseline,
+  Grid,
+  Toolbar,
+  Container,
+} from "@material-ui/core";
 
 function App() {
   return (
     <div className="App">
-      <Typography>Material UI</Typography>
+      <Typography variant="h1">Material UI</Typography>
     </div>
   );
 }
