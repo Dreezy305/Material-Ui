0863a5570443a87012dbab273fa12f447c546944
Dreezy305 <bankoleidris@gmail.com>
2021-08-07 12:50:47 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import { Typography } from "@material-ui/core/Typography";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Typography>Material UI</Typography>
+    </div>
+  );
 }
 
 export default App;
