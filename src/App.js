e53b194de7e6b2781bdf6104e518796c76727d83
Dreezy305 <bankoleidris@gmail.com>
2021-08-07 15:06:22 +0000

    useStyles and makeStyles

--- src/App.js
+++ src/App.js
@@ -14,8 +14,11 @@ import {
 import PhotoCameraIcon from "@material-ui/icons/PhotoCamera";
 import { makeStyles } from "@material-ui/core/styles";
 
-const useStyles = makeStyles(() => ({
-  container: {},
+const useStyles = makeStyles((theme) => ({
+  container: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(8, 0, 6),
+  },
 }));
 
 function App() {
