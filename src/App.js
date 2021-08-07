9080b7a8cd0e13385de68a047188dc8379f49409
Dreezy305 <bankoleidris@gmail.com>
2021-08-07 13:43:45 +0000

    importing components

--- src/App.js
+++ src/App.js
@@ -11,6 +11,8 @@ import {
   Container,
 } from "@material-ui/core";
 import PhotoCameraIcon from "@material-ui/icons/PhotoCamera";
+import Button from "@material-ui/core/Button";
+import ButtonGroup from "@material-ui/core/ButtonGroup";
 
 function App() {
   return (
@@ -29,9 +31,30 @@ function App() {
               variant="h2"
               align="center"
               color="textPrimary"
-            ></Typography>
+              gutterBottom
+            >
+              Photo Album
+            </Typography>
+            <Typography
+              variant="h5"
+              align="center"
+              color="textSecondary"
+              paragraph
+            >
+              Hello This is a Photo Abum, i am making this look as long as
+              possible so we can see how it looks like the browser
+            </Typography>
           </Container>
         </div>
+        <ButtonGroup
+          variant="contained"
+          color="primary"
+          aria-label="contained primary button group"
+        >
+          <Button>One</Button>
+          <Button>Two</Button>
+          <Button>Three</Button>
+        </ButtonGroup>
       </main>
     </>
   );
